9844cd963809ef149014a76bd3cf1714d284c757
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-17 12:00:03 -0300

    Typescript

---
+++ script.ts
@@ -0,0 +1,33 @@
+let sliderElement = document.querySelector("#slider")!;
+let buttonElement = document.querySelector("#button")!;
+
+let SizePassword = document.querySelector("#valor")!;
+let Passaword = document.querySelector("#password")!;
+
+let ContainerPassword = document.querySelector("#container-password")!;
+
+let chartSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&%";
+let novasenha = "";
+
+SizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    SizePassword.innerHTML = this.value 
+}
+
+function gerarSenha() {
+let pass = "";
+
+for(i = 0, n = chartSet.length; i < sliderElement.value; ++i){
+    pass += chartSet.charAt(Math.floor(Math.random() * n ));
+    
+}
+ContainerPassword.classList.remove("hide");
+ Passaword.innerHTML = pass
+ novasenha = pass
+}
+
+function copiarSenha() {
+    alert("SENHA COPIADA COM SUCESSO")
+    navigator.clipboard.writeText(novasenha);
+}
